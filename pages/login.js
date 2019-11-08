c2006dfb18615330c72a5755ddc211f21baec7e1
Muaykhao86 <waveflow18@gmail.com>
2019-11-08 13:55:52 +0000

    working auth

--- pages/login.js
+++ pages/login.js
@@ -60,35 +60,42 @@ constructor(props) {
             password: password,
         }
 
-    try{
-        const response = await fetch('https://prelude.eurobrake.net/login', {
-                method: 'post',
-                headers: {
-                    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-                },
-                credentials: 'include',
-                body: JSON.stringify(formData)
-            });
-        
-        response.json().then(function(data){
-            console.log(data);
+        const res = await fetch('https://prelude.eurobrake.net/login', {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(formData)
         })
-        if(response.status === 'success') {             //!Im not getting a status of success or error, im getting a response of 200 for both err and success
-        const {logintoken} = await response.json()
-        console.log({logintoken})
-        await login({logintoken})
-    }else{
-        console.log('login failed')
-        let error = new Error(response.statusText)
-        error.response = response
-       throw error
-        }
-    }catch (error){
-        console.error(
-            'Failed to login, please try again', error
-            )
-       this.setState({error: error.message});
-    }
+        const data = await res.json();
+        console.log(data);
+    // try{
+    //     const response = await fetch('https://prelude.eurobrake.net/login', {
+    //             method: 'post',
+    //             headers: {
+    //                 "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    //             },
+    //             credentials: 'include',
+    //             body: JSON.stringify(formData)
+    //         });
+        
+    //     response.json().then(function(data){
+    //         console.log(data);
+    //     })
+    //     if(response.status === 'success') {             //!Im not getting a status of success or error, im getting a response of 200 for both err and success
+    //     const {logintoken} = await response.json()
+    //     console.log({logintoken})
+    //     await login({logintoken})
+    // }else{
+    //     console.log('login failed')
+    //     let error = new Error(response.statusText)
+    //     error.response = response
+    //    throw error
+    //     }
+    // }catch (error){
+    //     console.error(
+    //         'Failed to login, please try again', error
+    //         )
+    //    this.setState({error: error.message});
+    // }
 
 }
 
