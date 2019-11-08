9798820ec551b7410e965a5a78bb983bbcfd27cd
Muaykhao86 <waveflow18@gmail.com>
2019-11-08 12:43:08 +0000

    working auth

--- pages/authorsArea.js
+++ pages/authorsArea.js
@@ -65,10 +65,10 @@ class AuthorsArea extends Component {
 AuthorsArea.getInitialProps = async ctx => {
     // We use `nextCookie` to get the cookie and pass the token to the
     // frontend in the `props`.
-    const {name} = nextCookie(ctx)
-    console.log(name);
+    const {Name} = nextCookie(ctx)
+    console.log(Name);
     const apiUrl = 'https://prelude.eurobrake.net/dashboard ';
-    console.log({logintoken})
+    // console.log({logintoken})
     // ? Dont think I need thi as Im not running a seperste server, so i think we can get away wuth just router.push
     const redirectOnError = () =>
         process.browser
