dda9573e3c468d8800d7d3d7d7773ee9aa1b998d
Muaykhao86 <waveflow18@gmail.com>
2019-11-11 13:47:29 +0000

    logging all cookies, passing credentials but not allowing fetch

--- pages/authorsArea.js
+++ pages/authorsArea.js
@@ -66,7 +66,9 @@ AuthorsArea.getInitialProps = async ctx => {
     // We use `nextCookie` to get the cookie and pass the token to the
     // frontend in the `props`.
     const {logintoken} = cookies(ctx);
+    const allCookies = cookies(ctx);
     console.log({logintoken}, 'getIProps');
+    console.log({allCookies});
     const apiUrl = 'https://prelude.eurobrake.net/dashboard ';
     // console.log({logintoken})
     // ? Dont think I need thi as Im not running a seperste server, so i think we can get away wuth just router.push
