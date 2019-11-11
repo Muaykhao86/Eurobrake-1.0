71092cbf0d84a34b9ff613ae0d55ad9899d08866
Muaykhao86 <waveflow18@gmail.com>
2019-11-11 11:16:13 +0000

    working auth

--- pages/authorsArea.js
+++ pages/authorsArea.js
@@ -83,7 +83,7 @@ AuthorsArea.getInitialProps = async ctx => {
     //   }
         })
         const data = await response.json();
-        console.log('Authors response Data =>', data)
+        console.log('Authors response Data =>', data.status, data)
         if (data.status === 'success') {
             const js = await response.json()
             console.log('js', js)
