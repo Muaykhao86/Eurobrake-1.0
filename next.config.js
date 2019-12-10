0461a68dbde517bbd8abde313040a57df1b73fdc
Muaykhao86 <waveflow18@gmail.com>
2019-12-10 16:06:38 +0000

    form testing

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = withCss({
         test: require.resolve('snapsvg/dist/snap.svg.js'),
         use: 'imports-loader?this=>window,fix=>module.exports=0',
       });
-    consfig.resolve.alias =  {
+    config.resolve.alias =  {
             snapsvg: 'snapsvg/dist/snap.svg.js',
           }
     return config
