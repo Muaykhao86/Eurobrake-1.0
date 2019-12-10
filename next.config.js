e93cc1f5bf12edaf2d56e5d5cb6708a19f318cc8
Muaykhao86 <waveflow18@gmail.com>
2019-12-10 15:42:25 +0000

    form testing

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = withCss({
         test: require.resolve('snapsvg/dist/snap.svg.js'),
         use: 'imports-loader?this=>window,fix=>module.exports=0'
       });
-    config.module.push(
+    config.push(
       {
         resolve: {
           alias: {
