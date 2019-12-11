03d9e005d9c9cc29cab9c9a9f8c535ee59465e78
Muaykhao86 <waveflow18@gmail.com>
2019-12-11 12:39:35 +0000

    form testing

--- pages/authorsAreaDash.js
+++ pages/authorsAreaDash.js
@@ -273,7 +273,10 @@ class AuthorsAreaDash extends Component {
                         </div>
                         <Typography gutterBottom className="authors-title" style={{ fontSize: '3rem', borderTop: '2px solid #134381', borderBottom: '2px solid #134381' }}>Your Abstracts</Typography>
                         {AreAbstracts ?
-                            <Abstract /> :
+                            <StyledBox>
+                                <Typography gutterBottom className="authors-it" style={{ fontSize: '2rem' }}>You got some abstracts but I got some errors!</Typography>
+                            </StyledBox>
+                             :
                             <StyledBox>
                                 <Typography gutterBottom className="authors-it" style={{ fontSize: '2rem' }}>You haven't submitted any abstracts yet</Typography>
                             </StyledBox>
@@ -288,6 +291,7 @@ class AuthorsAreaDash extends Component {
 
     }
 }
+                            {/* <Abstract /> */}
 
 AuthorsAreaDash.getInitialProps = async ctx => {
     // We use `nextCookie` to get the cookie and pass the token to the frontend in the `props`.
