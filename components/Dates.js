fd4e6422b0426a00b8cfc1b693a33392fcbbe644
Muaykhao86 <waveflow18@gmail.com>
2020-01-02 13:28:04 +0000

    still passing paper code

--- components/Dates.js
+++ components/Dates.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
-import Card from './Card';
+import {Card} from './Card';
 
 
 export default function Dates() {
