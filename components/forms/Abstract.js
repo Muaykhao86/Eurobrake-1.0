c0d68a7873718ecdacf4e39a66e7a32a32743f2c
Muaykhao86 <waveflow18@gmail.com>
2020-01-23 19:41:21 +0000

    can test all forms now and contact and withdraw but NOT newsletter=> didnt save

--- components/forms/Abstract.js
+++ components/forms/Abstract.js
@@ -31,7 +31,7 @@ export const AbstractForm = (props) => {
             validationSchema={AbstractSchema}
             enableReinitialize
         >
-            {({ values, isSubmitting, isValidating, handleChange, setFieldValue, setFieldTouched }) => {
+            {({ values, isSubmitting, isValidating, handleChange, setFieldValue, setFieldTouched, validateForm }) => {
           
                 const allNo = ({name}) => {
                     if(process.browser){
